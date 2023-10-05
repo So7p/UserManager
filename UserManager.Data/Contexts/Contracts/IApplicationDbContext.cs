c79efbb81af78467add061dd239e60e4ebdabc85
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-10-05 15:20:49 +0300

    Configure database context

---
+++ UserManager.Data/Contexts/Contracts/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManager.Data.Entities;
+
+namespace UserManager.Data.Contexts.Contracts
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Role> Roles { get; set;}
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ UserManager.Data/Contexts/Implementation/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using UserManager.Data.Contexts.Contracts;
+using UserManager.Data.Entities;
+
+namespace UserManager.Data.Contexts.Implementation
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Role> Roles { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public async Task<int> SaveChangesAsync() => await base.SaveChangesAsync();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
