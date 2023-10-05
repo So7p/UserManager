0f260dd210b9fe3d2c3a7c78d0e3029423fd262f
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-10-05 15:23:29 +0300

    Add ServiceExtensions

---
+++ UserManager.Data/IoC/ServiceExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserManager.Data.Contexts.Contracts;
+using UserManager.Data.Contexts.Implementation;
+using UserManager.Data.Repositories.Contracts;
+using UserManager.Data.Repositories.Implementation;
+
+namespace UserManager.Data.IoC
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("sqlDbConnection")),
+            ServiceLifetime.Transient);
+        }
+
+        public static void ConfigureDbContext(this IServiceCollection services) 
+        {
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
+
+            return services;
+        }
+    }
+}
