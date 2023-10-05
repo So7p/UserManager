bbd7713efbb04a7bed054c5f7552e935e72df18c
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-10-05 15:17:26 +0300

    Create entities and configurations

---
+++ UserManager.Data/Entities/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManager.Data.Entities
+{
+    public class Role : BaseEntity
+    {
+        public string RoleName { get; set; } = null!;
+
+        public IEnumerable<User> Users { get; set; } = null!;   
+    }
+}
