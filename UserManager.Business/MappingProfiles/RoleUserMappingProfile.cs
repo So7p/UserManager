7dad9af4975cb8f1f0104304bf7436b47d872d83
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-10-08 18:27:55 +0300

    Add mapping profiles

---
+++ UserManager.Business/MappingProfiles/RoleUserMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using UserManager.Business.DTOs.RoleUser;
+using UserManager.Data.Entities;
+
+namespace UserManager.Business.MappingProfiles
+{
+    public class RoleUserMappingProfile : AutoMapper.Profile
+    {
+        public RoleUserMappingProfile() 
+        {
+            CreateMap<RoleUser, RoleUserForViewDto>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.Name))
+                .ForMember(dest => dest.UserAge, opt => opt.MapFrom(src => src.User.Age))
+                .ForMember(dest => dest.UserEmail, opt => opt.MapFrom(src => src.User.Email))
+                .ForMember(dest => dest.UserRoleName, opt => opt.MapFrom(src => src.Role.RoleName));
+
+            CreateMap<RoleUserForCreationDto, RoleUser>();
+            CreateMap<RoleUserForUpdateDto, RoleUser>();
+        }
+    }
+}
---
+++ UserManager.Business/MappingProfiles/UserMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using UserManager.Business.DTOs.User;
+using UserManager.Data.Entities;
+
+namespace UserManager.Business.MappingProfiles
+{
+    public class UserMappingProfile : AutoMapper.Profile
+    {
+        public UserMappingProfile() 
+        {
+            CreateMap<User, UserForViewDto>();
+            CreateMap<UserForCreationDto, User>();
+            CreateMap<UserForUpdateDto, User>();
+        }
+    }
+}
