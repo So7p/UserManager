b0e5ebdfcd988d8bbe2fa70b1775cd0e71940871
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-10-08 18:28:33 +0300

    Add services

---
+++ UserManager.Business/Services/Contracts/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using UserManager.Business.DTOs.Role;
+
+namespace UserManager.Business.Services.Contracts
+{
+    public interface IRoleService
+    {
+        Task<IEnumerable<RoleForViewDto>> GetAllAsync();
+        Task<RoleForViewDto?> GetByIdAsync(int id);
+        Task CreateAsync(RoleForCreationDto roleForCreationDto);
+        Task UpdateAsync(RoleForUpdateDto roleForUpdateDto);
+        Task DeleteAsync(int id);
+    }
+}
