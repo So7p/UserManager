51e03c8b858ee22e214f0deb6ee21ea9db74abf8
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-10-09 18:10:41 +0300

    Add new methods to repositories

--- UserManager.Data/Repositories/Contracts/IUserRepository.cs
+++ UserManager.Data/Repositories/Contracts/IUserRepository.cs
@@ -4,6 +4,8 @@ namespace UserManager.Data.Repositories.Contracts
 {
     public interface IUserRepository : IBaseRepository<User>
     {
-        Task<User?> GetByEmailAsync(string email);
+        Task<IEnumerable<User>> GetByUserNameAsync(string userName);
+        Task<IEnumerable<User>> GetByAgeAsync(int age);
+        Task<User?> GetByEmailAsync(string email); 
     }
 }
