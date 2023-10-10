656af800ae8ff9b5c2d41b3bc42981459c3d5176
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-10-10 18:26:58 +0300

    Add logger to controllers

--- UserManager.WebApi/Controllers/RoleUserController.cs
+++ UserManager.WebApi/Controllers/RoleUserController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UserManager.Business.DTOs.RoleUser;
+using UserManager.Business.DTOs.User;
 using UserManager.Business.Services.Contracts;
+using UserManager.Data.Entities;
 
 namespace UserManager.WebApi.Controllers
 {
@@ -9,15 +11,19 @@ namespace UserManager.WebApi.Controllers
     public class RoleUserController : Controller
     {
         private readonly IRoleUserService _roleUserService;
+        private readonly ILogger<RoleUserController> _logger;
 
-        public RoleUserController(IRoleUserService roleUserService)
+        public RoleUserController(IRoleUserService roleUserService, ILogger<RoleUserController> logger)
         {
             _roleUserService = roleUserService;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllAsync()
         {
+            _logger.LogInformation("Requested all RoleUsers");
+
             var roleUsers = await _roleUserService.GetAllAsync();
 
             return Ok(roleUsers);
@@ -26,10 +32,18 @@ namespace UserManager.WebApi.Controllers
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetByIdAsync([FromRoute] int id)
         {
+            _logger.LogInformation("Requested RoleUser by Id {id}", id);
+
             var roleUser = await _roleUserService.GetByIdAsync(id);
 
             if (roleUser == null)
+            {
+                _logger.LogInformation("RoleUser was not found by Id {id}", id);
+
                 return NotFound();
+            }
+
+            _logger.LogInformation("RoleUser successfully found by Id {id}: {@roleUser}", id, roleUser);
 
             return Ok(roleUser);
         }
@@ -37,7 +51,11 @@ namespace UserManager.WebApi.Controllers
         [HttpGet("getByUserId/{userId:int}")]
         public async Task<IActionResult> GetByUserIdAsync([FromRoute] int userId)
         {
+            _logger.LogInformation("Requested RoleUsers by User Id {userId}", userId);
+
             var roleUsers = await _roleUserService.GetByUserIdAsync(userId);
+
+            _logger.LogInformation("RoleUsers found by User Id {userId}: {@roleUsers}", userId, roleUsers);
 
             return Ok(roleUsers);
         }
@@ -45,7 +63,11 @@ namespace UserManager.WebApi.Controllers
         [HttpGet("getByUserName/{userName}")]
         public async Task<IActionResult> GetByUserNameAsync([FromRoute] string userName)
         {
+            _logger.LogInformation("Requested RoleUsers by Username {userName}", userName);
+
             var roleUsers = await _roleUserService.GetByUserNameAsync(userName);
+
+            _logger.LogInformation("RoleUsers found by Username {userName}: {@roleUsers}", userName, roleUsers);
 
             return Ok(roleUsers);
         }
@@ -53,7 +75,11 @@ namespace UserManager.WebApi.Controllers
         [HttpGet("getByUserAge/{userAge:int}")]
         public async Task<IActionResult> GetByUserAgeAsync([FromRoute] int userAge)
         {
+            _logger.LogInformation("Requested RoleUsers by User Age {userAge}", userAge);
+
             var roleUsers = await _roleUserService.GetByUserAgeAsync(userAge);
+
+            _logger.LogInformation("RoleUsers found by User Age {userAge}: {@roleUsers}", userAge, roleUsers);
 
             return Ok(roleUsers);
         }
@@ -61,7 +87,11 @@ namespace UserManager.WebApi.Controllers
         [HttpGet("getByUserEmail/{userEmail}")]
         public async Task<IActionResult> GetByUserEmail([FromRoute] string userEmail)
         {
+            _logger.LogInformation("Requested RoleUsers by User Email {userEmail}", userEmail);
+
             var roleUsers = await _roleUserService.GetByUserEmailAsync(userEmail);
+
+            _logger.LogInformation("RoleUsers found by User Email {userEmail}: {@roleUsers}", userEmail, roleUsers);
 
             return Ok(roleUsers);
         }
@@ -69,7 +99,11 @@ namespace UserManager.WebApi.Controllers
         [HttpGet("getByUserRoleName/{userRoleName}")]
         public async Task<IActionResult> GetByUserRoleName([FromRoute] string userRoleName)
         {
+            _logger.LogInformation("Requested RoleUsers by User Role {userRoleName}", userRoleName);
+
             var roleUsers = await _roleUserService.GetByUserRoleNameAsync(userRoleName);
+
+            _logger.LogInformation("RoleUsers found by User Role {userRoleName}: {@roleUsers}", userRoleName, roleUsers);
 
             return Ok(roleUsers);
         }
@@ -78,9 +112,15 @@ namespace UserManager.WebApi.Controllers
         public async Task<IActionResult> CreateAsync([FromForm] RoleUserForCreationDto roleUserForCreationDto)
         {
             if (!ModelState.IsValid)
+            {
+                _logger.LogInformation("RoleUser data was not valid: {@roleUserForCreationDto}", roleUserForCreationDto);
+
                 return BadRequest();
+            }   
 
             await _roleUserService.CreateAsync(roleUserForCreationDto);
+
+            _logger.LogInformation("New RoleUser created: {@roleUserForCreationDto}", roleUserForCreationDto);
 
             return StatusCode(201);
         }
@@ -89,9 +129,15 @@ namespace UserManager.WebApi.Controllers
         public async Task<IActionResult> UpdateAsync([FromForm] RoleUserForUpdateDto roleUserForUpdateDto)
         {
             if (!ModelState.IsValid)
+            {
+                _logger.LogInformation("User data was not valid: {@roleUserForUpdateDto}", roleUserForUpdateDto);
+
                 return BadRequest();
+            }
 
             await _roleUserService.UpdateAsync(roleUserForUpdateDto);
+
+            _logger.LogInformation("User data updated to: {@roleUserForUpdateDto}", roleUserForUpdateDto);
 
             return NoContent();
         }
@@ -100,6 +146,8 @@ namespace UserManager.WebApi.Controllers
         public async Task<IActionResult> DeleteAsync([FromRoute] int id)
         {
             await _roleUserService.DeleteAsync(id);
+
+            _logger.LogInformation("RoleUser deleted by Id {id}", id);
 
             return NoContent();
         }
